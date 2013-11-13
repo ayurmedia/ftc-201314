e2e61d9eed1fadef7add371d43aa041a297dd176
Silu Tang <silu.na.tang@gmail.com>
2013-11-12 22:04:39 -0500

    Light sensor autonomous
    Failed attempt on 11/9 to use light sensor in autonomous. Ditching this
    idea, because it doesn't work.

--- Code for Actual Robot/BaseAutonomous.c
+++ Code for Actual Robot/BaseAutonomous.c
@@ -5,7 +5,6 @@
 #pragma config(Motor,  mtr_S1_C1_2,     dt_right,      tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C2_1,     flag_raiser,   tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C2_2,     archemedes,    tmotorTetrix, openLoop)
-#pragma config(Servo,  srvo_S1_C3_1,    auto_block,           tServoStandard)
 #pragma config(Servo,  srvo_S1_C3_2,    UNUSED1,              tServoStandard)
 #pragma config(Servo,  srvo_S1_C3_3,    UNUSED2,              tServoStandard)
 #pragma config(Servo,  srvo_S1_C3_4,    UNUSED3,              tServoStandard)
@@ -13,21 +12,46 @@
 #pragma config(Servo,  srvo_S1_C3_6,    UNUSED5,              tServoStandard)
 
 #include "JoystickDriver.c"
-
 // Constants for light sensor
 static const int GRAY_LIGHT_VAL = 0;
 static const int BLUE_LIGHT_VAL = 0;
 static const int RED_LIGHT_VAL = 0;
+static const int WHITE_LIGHT_VAL = 0;
 static const int LIGHT_TOLERANCE = 5;
+static const int THRESHOLD = 5;
+static const int IR_LOCATION = 5;
+static const int MAX_POWER = 80;
+static const int MAX_SERVO_ANGLE = 120;
+static const int TIME_ON_RAMP = 5;
+int timer = 0;
+
+void moveForward()
+{
+  motor[dt_right] = MAX_POWER;
+  motor[dt_left] = MAX_POWER;
+}
+
+void moveRight()
+{
+  motor[dt_right] = -MAX_POWER;
+  motor[dt_left] = MAX_POWER;
+  wait1MSec(1000);
+}
+
+void promptForInputs() {
+
+
+}
 
 
 // Positions servo for block-dispensing manipulator
 void initializeRobot() {
 	servo[auto_block] = 0;
+  promptForInputs();
+
 
   return;
 }
-
 
 task main() {
   initializeRobot();
@@ -38,9 +62,9 @@ task main() {
     k_findColoredLine = 0,
     k_findIR,
     k_dispenseBlock,
-    k_followLine,
+    k_followColoredLine,
+    k_findWhiteLine,
     k_getOnRamp,
-    k_turn90,
     k_finish
   } State;
   State m_state = k_findColoredLine;
@@ -48,14 +72,22 @@ task main() {
   while(m_state != k_finish) {
   	switch(m_state) {
   		case k_findColoredLine:
-  			// *** May use encoders
   			// IF: found colored line, m_state++;
   			// ELSE: drive straight
+        while(SensorValue[light] - GRAY_LIGHT_VAL <= THRESHOLD)
+        {
+          moveForward();
+        }
   		break;
 
   		case k_findIR:
   			// IF: found/centered IR beacon, STOP DRIVETRAIN then m_state++;
   			// ELSE; follow line & drive forward
+      while((SensorValue[light] - BLUE_LIGHT_VAL <= THRESHOLD || SensorValue[light] - RED_LIGHT_VAL <= THRESHOLD) && SensorValue[ir_seeker] != IR_LOCATION)
+      {
+          moveForward();
+      }
+      servo[auto_block] = MAX_SERVO_ANGLE;
   		break;
 
   		case k_dispenseBlock:
@@ -63,16 +95,31 @@ task main() {
   			// ELSE: move servo
   		break;
 
-  		case k_followLine:
+  		case k_followColoredLine:
   			// IF: colored line ends, m_state++;
   			// ELSE: light sensor line-following
+      while(SensorValue[light] - BLUE_LIGHT_VAL <= THRESHOLD || SensorValue[light] - RED_LIGHT_VAL <= THRESHOLD)
+      {
+        moveForward();
+      }
+      while
+  		break;
+
+  		case k_findWhiteLine:
+      turnRight();
+      while(SensorValue[light] - GRAY_LIGHT_VAL <= THRESHOLD)
+      {
+        moveForward();
+      }
+      turnRight();
+      while(SensorValue[light] - WHITE_LIGHT_VAL <= THRESHOLD && timer <= TIME_ON_RAMP)
+      {
+        moveForward();
+        timer++;
+      }
   		break;
 
   		case k_getOnRamp:
-
-  		break;
-
-  		case k_turn90:
 
   		break;
   	}
