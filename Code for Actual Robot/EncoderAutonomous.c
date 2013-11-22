c400577411d9eeeeee2c78e70f615d4a90f1fe6b
Silu Tang <silu.na.tang@gmail.com>
2013-11-21 19:45:28 -0500

    Clean up autonomous

--- Code for Actual Robot/EncoderAutonomous.c
+++ Code for Actual Robot/EncoderAutonomous.c
@@ -1,21 +1,16 @@
-#pragma config(Hubs,  S1, HTMotor,  HTMotor,  HTServo,  none)
+#pragma config(Hubs,  S1, HTMotor,  HTServo,  none,     none)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
 #pragma config(Sensor, S2,     ir_seeker,      sensorHiTechnicIRSeeker600)
-#pragma config(Motor,  mtr_S1_C1_1,     dt_left,       tmotorTetrix, PIDControl, encoder)
-#pragma config(Motor,  mtr_S1_C1_2,     dt_right,      tmotorTetrix, PIDControl, reversed, encoder)
-#pragma config(Motor,  mtr_S1_C2_1,     flag_raiser,   tmotorTetrix, openLoop)
-#pragma config(Motor,  mtr_S1_C2_2,     archemedes,    tmotorTetrix, openLoop)
-#pragma config(Servo,  srvo_S1_C3_1,    auto_block,           tServoStandard)
-#pragma config(Servo,  srvo_S1_C3_2,    UNUSED1,              tServoStandard)
-#pragma config(Servo,  srvo_S1_C3_3,    UNUSED2,              tServoStandard)
-#pragma config(Servo,  srvo_S1_C3_4,    UNUSED3,              tServoStandard)
-#pragma config(Servo,  srvo_S1_C3_5,    UNUSED4,              tServoStandard)
-#pragma config(Servo,  srvo_S1_C3_6,    UNUSED5,              tServoStandard)
+#pragma config(Motor,  mtr_S1_C1_1,     dt_left,       tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C1_2,     dt_right,      tmotorTetrix, openLoop, reversed)
+#pragma config(Servo,  srvo_S1_C2_1,    auto_block,           tServoStandard)
 
-const static int IR_SEEKING_VEL = 50;
-const static int TOTAL_BRIDGE_DIST = 50;
+
+const static int IR_SEEKING_VEL = 30;
+const static int TOTAL_BRIDGE_DIST = 20;
 const static int SERVO_CHANGE_RATE = 20;
 const static int ARM_EXTENDED_POS = 90;
+const static int SERVO_STOP_TIME = 1000;
 const static int ARM_RETRIEVED_POS = 0;
 const static int MAX_VEL = 80;
 const static int CENTER_BRIDGE_DIST = 100;
@@ -26,19 +21,20 @@ void initializeRobot() {
   // Make sure all motors are not moving before waitForStart()
   motor[dt_left] = 0;
   motor[dt_right] = 0;
-  nMotorEncoder[dt_left] = 0;
+  nMotorEncoder[dt_left] = 0; 
 
   // Set default speed for the auto_block servo
-  servoChangeRate[auto_block] = 0;                   // TODO: Run test to find magic number
+  servoChangeRate[auto_block] = 0;
   // Set the initial position for the auto_block servo
-  servo[auto_block] = ARM_RETRIEVED_POS;            // TODO: Run test to find magic number
+  servo[auto_block] = ARM_RETRIEVED_POS;
 
   return;
 }
 
 void moveDT(int a_speed) {
+	motor[dt_right] = a_speed;
   motor[dt_left] = a_speed;
-  motor[dt_right] = a_speed;
+
 }
 
 void moveServoTo(short a_name, int a_position) {
@@ -49,23 +45,23 @@ void moveServoTo(short a_name, int a_position) {
   }
 }
 
-// TODO: Run test to calibrate this
 int inchesToEncoder(float a_inches) {
-  const static int  ticksPerRotation = 1440;
-  const static float wheelCircumference = 4 * PI;
+  const static int  ticksPerRotation = 1350;
+  const static float wheelCircumference = 3 * PI;
 
   return a_inches * ticksPerRotation / wheelCircumference;
 }
 
-// TODO: figure out how to actually do this
 bool turnRight() {
 	const static int robotDiameter = 18;
 	const static int turnDist = robotDiameter * PI / 4;
 	const static int turnVel = 80;
 
-	motor[dt_left] = turnVel;
-	motor[dt_right] = -turnVel;
-	while(nMotorEncoder[dt_left] < turnDist) {}
+	motor[dt_left] = -turnVel;
+	motor[dt_right] = turnVel;
+	while(abs(nMotorEncoder[dt_left]) < inchesToEncoder(turnDist)) {
+    writeDebugStream("turnRight: %d", nMotorEncoder[dt_left]);
+  }
 	moveDT(0);
 
   return false;
@@ -92,12 +88,15 @@ task main() {
 
   // Main state machine
   while(m_state != kFinishAuto) {
+  	writeDebugStream("State %d: ", m_state);
     switch(m_state) {
       // Move forward until IR seeker centers the IR beacon
       case kFindIR:
+      	writeDebugStream("ir_seeker: %d", SensorValue[ir_seeker]);
         if(SensorValue[ir_seeker] != 5) {
           moveDT(IR_SEEKING_VEL);
         } else {
+        	wait10Msec(50);
           moveDT(0);
           m_state++;
         }
@@ -105,15 +104,18 @@ task main() {
 
       // Moves auto_block servo to a certain position
       case kDispenseBlock:
-        if(ServoValue[auto_block] != ARM_EXTENDED_POS) {
+      	writeDebugStream("servoValue: %d", ServoValue[auto_block]);
+        if(ServoValue[auto_block] <= ARM_EXTENDED_POS) {
           moveServoTo(auto_block, ARM_EXTENDED_POS);
         } else {
+        	wait1Msec(SERVO_STOP_TIME);
           m_state++;
         }
       break;
 
       // Pulls "arm" back to orgiinal position after dispensing the autonomous block
       case kRetrieveArm:
+      	writeDebugStream("servoValue: %d", ServoValue[auto_block]);
         if(ServoValue[auto_block] != ARM_RETRIEVED_POS) {
           moveServoTo(auto_block, ARM_RETRIEVED_POS);
         } else {
@@ -123,7 +125,8 @@ task main() {
 
       // Move straight forward a certain number of inches
       case kPassBridge:
-        if(nMotorEncoder[dt_left] < inchesToEncoder(TOTAL_BRIDGE_DIST)) {
+      	writeDebugStream("kPassBridge: %d", nMotorEncoder[dt_left] );
+        if(abs(nMotorEncoder[dt_left]) < inchesToEncoder(TOTAL_BRIDGE_DIST)) {
           moveDT(MAX_VEL);
         } else {
           moveDT(0);
@@ -134,14 +137,15 @@ task main() {
 
       // Turn right 90 degrees
       case kTurnRight1:
-        turnRight();    // TODO: Actually figure out how to do this
+        turnRight(); 
         nMotorEncoder[dt_left] = 0;
         m_state++;
       break;
 
       // Move straight forward a certain number of inches
       case kCenterBridge:
-        if(nMotorEncoder[dt_left] < inchesToEncoder(CENTER_BRIDGE_DIST)) {
+      	writeDebugStream("kCenterBridge: %d", nMotorEncoder[dt_left]);
+        if(abs(nMotorEncoder[dt_left]) < inchesToEncoder(CENTER_BRIDGE_DIST)) {
           moveDT(MAX_VEL);
         } else {
           moveDT(0);
@@ -159,7 +163,8 @@ task main() {
 
       // Move straight forward a certain number of encoder ticks
       case kGetOnRamp:
-        if(nMotorEncoder[dt_left] < inchesToEncoder(RAMP_DIST)) {
+     		writeDebugStream("kGetOnRamp - %d", nMotorEncoder[dt_left]);
+        if(abs(nMotorEncoder[dt_left]) < inchesToEncoder(RAMP_DIST)) {
           moveDT(MAX_VEL);
         } else {
           moveDT(0);
@@ -171,9 +176,14 @@ task main() {
       // Indicates the end of the autonomous program
       default:
       break;
+
+      writeDebugStreamLine("");
   }
 }
 
+// Log - State machines may be well organized, but for what you are trying to do here, it may be overcomplicating the process.
+//       This may work perfectly well for you, but if you start to encounter issues that you are having difficulty finding the reason for,
+//       you may want to consider using a more straightforward structure for this. That being said, it looks good for what you said it needs to do.
 
   // "Special" RobotC thing where it needs a forever loop at the end of the autonomous program to wait for the end of the auonomous period
   for(;/*ever*/;) {}
